55bca3a67ea64b38f92a06529f266476a069a880
Taranchuk <nfhfyxer@gmail.com>
2026-08-16 20:06:11 +0300

    cot fix

--- 1.6/Source/PlaceWorker_WallCot.cs
+++ 1.6/Source/PlaceWorker_WallCot.cs
@@ -3,22 +3,23 @@ using Verse;
 
 namespace CaptureExpansion
 {
+    [HotSwappable]
     public class PlaceWorker_WallCot : PlaceWorker
     {
         public override AcceptanceReport AllowsPlacing(BuildableDef checkingDef, IntVec3 loc, Rot4 rot, Map map, Thing thingToIgnore = null, Thing thing = null)
         {
-            var dir = rot.FacingCell.RotatedBy(Rot4.East);
-            var p1 = loc - dir;
-            var p2 = loc + dir;
-
-            if (IsWall(p1, map) is false || IsWall(p2, map) is false)
+            var rect = GenAdj.OccupiedRect(loc, rot, checkingDef.Size);
+            foreach (var cell in rect.GetEdgeCells(rot))
             {
-                return "CE_RequiresWallsFirstAndThird".Translate();
+                if (IsWall(cell + rot.FacingCell.RotatedBy(RotationDirection.Counterclockwise), map) is false)
+                {
+                    return "CE_RequiresWalls".Translate();
+                }
             }
             return true;
         }
 
-        private bool IsWall(IntVec3 c, Map map)
+        private static bool IsWall(IntVec3 c, Map map)
         {
             if (c.InBounds(map) is false) return false;
             var edifice = c.GetEdifice(map);
