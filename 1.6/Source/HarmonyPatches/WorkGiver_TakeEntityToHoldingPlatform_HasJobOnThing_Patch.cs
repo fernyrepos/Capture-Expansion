a5c66d1ad809cc41da5970e8e25b0ab2c33feb55
Taranchuk <nfhfyxer@gmail.com>
2026-08-20 01:38:19 +0300

    fix

--- 1.6/Source/HarmonyPatches/WorkGiver_TakeEntityToHoldingPlatform_HasJobOnThing_Patch.cs
+++ 1.6/Source/HarmonyPatches/WorkGiver_TakeEntityToHoldingPlatform_HasJobOnThing_Patch.cs
@@ -7,12 +7,20 @@ namespace CaptureExpansion
     [HarmonyPatch(typeof(WorkGiver_TakeEntityToHoldingPlatform), nameof(WorkGiver_TakeEntityToHoldingPlatform.HasJobOnThing))]
     public static class WorkGiver_TakeEntityToHoldingPlatform_HasJobOnThing_Patch
     {
-        public static void Postfix(Thing t, ref bool __result)
+        public static bool Prefix(Thing t, ref bool __result)
         {
-            if (__result && t is Pawn { IsPrisonerOfColony: true })
+            var comp = t.TryGetComp<CompHoldingPlatformTarget>();
+            if (comp?.targetHolder != null && comp.targetHolder.TryGetComp<CompEntityHolder>() == null)
             {
                 __result = false;
+                return false;
             }
+            if (t is Pawn { IsPrisonerOfColony: true })
+            {
+                __result = false;
+                return false;
+            }
+            return true;
         }
     }
 }
